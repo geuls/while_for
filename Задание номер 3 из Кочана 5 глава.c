cd784d5ed7c7331b4a321d762679886a0416217e
geuls <32553087+geuls@users.noreply.github.com>
2018-03-30 23:54:01 +0300

    Rename Задание номер 3 из Прата 5 глава.c to Задание номер 3 из Кочана 5 глава.c

---
+++ Задание номер 3 из Кочана 5 глава.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include "stdafx.h"
+
+
+int main (void)
+{
+	int triangularNumber = 0;
+	int n;
+	for (n = 0; n <= 50; n = n + 5)
+	{
+		triangularNumber = (n * (n + 1)) / 2;
+		printf("triangularNumber is %i\n", triangularNumber);
+	};
+	return 0;
+}
+
